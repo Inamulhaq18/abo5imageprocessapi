6e8d21e9f6378a9e97cd766588bafa4caa0bf40f
Syed Inaamul Haq <37792859+Inamulhaq18@users.noreply.github.com>
2022-08-24 16:13:29 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,27 +4,10 @@ import time
 from threading import Thread
 from app.bgprocess import *
 
+print("inside")
+
 app = Flask(__name__)
 
 @app.route("/")
-def main():
+def home_view():
     return "Welcome!"
-'''
-@app.route('/processBG')
-def return_status():
-    """Return first the response and tie the my_task to a thread"""
-    Rurl = (request.args.get('rurl'))
-    if len(Rurl)>0:
-        Thread(target = my_task,args=(Rurl,)).start()
-    if len(Rurl)==0:
-        return jsonify(str("No images"))
-    Rurl=""
-    return jsonify(str("Images detected"))
-
-def my_task(rurl):
-    print("rurl Before:  "+rurl)
-    bgprocess(rurl)
-    print("rurl After:  "+rurl)
-    rurl=""
-    print("rurl After:  "+rurl)
-    return print('large function completed')'''
