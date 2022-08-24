d706066f1c316d7c0564158b485629ecd260f988
Syed Inaamul Haq <37792859+Inamulhaq18@users.noreply.github.com>
2022-08-24 17:09:56 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ import os
 from flask import Flask,jsonify,request
 import time
 from threading import Thread
-import ayman 
+#import ayman 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return (ayman.say())
+    return ("ham homge kamiyab")
