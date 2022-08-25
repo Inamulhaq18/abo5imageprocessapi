b7b4c1bae12aa64b34d74c89601e447682c44da3
Syed Inaamul Haq <37792859+Inamulhaq18@users.noreply.github.com>
2022-08-25 12:22:18 +0300

    Update bgprocess.py

--- bgprocess.py
+++ bgprocess.py
@@ -12,6 +12,7 @@ import s3fs
 import os
 import datetime
 import psycopg2
+import gc
 
 conn=psycopg2.connect("postgresql://hkmuctkbhmlhsr:59563300aab6c650f8bbc9cc4153df6a42054b71e9be00dda420f40bbbf791b2@ec2-54-76-43-89.eu-west-1.compute.amazonaws.com:5432/dd8a5bspvhrk8c") 
 curr=conn.cursor()
@@ -108,6 +109,7 @@ def bgprocess(rurl):
     print("_________PURL_________")
     print(Purl)
     pushdbupdate(Rurls,Purl)
+    gc.collect()
 
     print("completed operation")
     return("done")
