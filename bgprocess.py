2da408579dda08551bac1f6733d82c495b2dd21d
Syed Inaamul Haq <37792859+Inamulhaq18@users.noreply.github.com>
2022-09-29 21:21:43 +0300

    Update bgprocess.py

--- bgprocess.py
+++ bgprocess.py
@@ -14,8 +14,6 @@ import datetime
 import psycopg2
 import gc
 
-conn=psycopg2.connect("postgresql://hkmuctkbhmlhsr:59563300aab6c650f8bbc9cc4153df6a42054b71e9be00dda420f40bbbf791b2@ec2-54-76-43-89.eu-west-1.compute.amazonaws.com:5432/dd8a5bspvhrk8c") 
-curr=conn.cursor()
 
 os.environ["AWS_DEFAULT_REGION"] = 'us-east-2'
 os.environ["AWS_ACCESS_KEY_ID"] = 'AKIARLFEN3ZYTWBVYNX7'
@@ -72,6 +70,8 @@ def pushtos3(img):
 
 
 def pushdbupdate(Rurls,purl):
+    conn=psycopg2.connect("postgresql://hkmuctkbhmlhsr:59563300aab6c650f8bbc9cc4153df6a42054b71e9be00dda420f40bbbf791b2@ec2-54-76-43-89.eu-west-1.compute.amazonaws.com:5432/dd8a5bspvhrk8c") 
+    curr=conn.cursor()
     sql_select_query = """UPDATE master_product_table SET "Product_image_P_url" = %s WHERE "Product_image_R_url" = %s"""
     print("__Purl__")
     purl=",".join(purl)
